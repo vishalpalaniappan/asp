a4a5f0bdbf110aca40b0154fa390bcb85f7b709a
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-04-17 23:14:47 -0400

    Save node file name to json file.

--- components/asp/TraceAssembler.py
+++ components/asp/TraceAssembler.py
@@ -38,7 +38,8 @@ class TraceAssembler:
         for ioEvent in log.decoder.systemIoNodes:
             if ioEvent["type"] == "start":
                 self.currentTrace = []
-                self.currentTrace.append(ioEvent["node"])
+                # self.currentTrace.append(ioEvent["node"])
+                self.currentTrace.append(log.decoder.header.programInfo["name"])
                 self.findTrace(ioEvent["node"])
                 self.traces.append(self.currentTrace)
 
@@ -52,18 +53,17 @@ class TraceAssembler:
 
         for log in self.system.logFiles:
             fileName = log.logFileName.split(".")[0]
-            if(fileName == searchId):
-                for ioEvent in log.decoder.systemIoNodes:
-                    
-                    if ioEvent["type"] == "link" or ioEvent["type"] == "end":
-                        node = ioEvent["node"]
-                        currId = node["adliExecutionId"]
-                        currIndex = node["adliExecutionIndex"]
+            for ioEvent in log.decoder.systemIoNodes:
+                
+                if ioEvent["type"] == "link" or ioEvent["type"] == "end":
+                    node = ioEvent["node"]
+                    currId = node["adliExecutionId"]
+                    currIndex = node["adliExecutionIndex"]
 
-                        if ((searchId == currId) and (searchIndex == currIndex)):
-                            if "output" in node:
-                                self.currentTrace.append(node)
-                                # TODO: Add support for processing multiple outputs.
-                                self.findTrace(node["output"][0])
-                            else:
-                                self.currentTrace.append(node)
+                    if ((searchId == currId) and (searchIndex == currIndex)):
+                        if "output" in node:
+                            self.currentTrace.append(log.decoder.header.programInfo["name"])
+                            # TODO: Add support for processing multiple outputs.
+                            self.findTrace(node["output"][0])
+                        else:
+                            self.currentTrace.append(log.decoder.header.programInfo["name"])
