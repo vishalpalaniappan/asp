67d48b54eddd9829dcde98b2257105afe0db5daa
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-04-21 16:44:35 -0400

    Add system database reader.

---
+++ components/query_handler/SystemDatabaseReader.py
@@ -0,0 +1,21 @@
+
+class SystemDatabaseReader:
+    '''
+        This class will perform queries on the ASP database and return
+        the results.
+    '''
+
+    def __init__(self):
+        pass
+
+    def getSystems(self):
+        pass
+
+    def getPrograms(self, systemId, systemVersion):
+        pass
+
+    def getDeployments(self, systemId, systemVersion):
+        pass
+
+    def getTraces(self, systemId, systemVersion, deploymentId):
+        pass
