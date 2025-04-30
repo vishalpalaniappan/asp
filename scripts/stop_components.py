702d247a84b13f20c32445624ec29618a06e5e09
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-04-30 16:59:39 -0400

    Improve exception handling when stopping components.

--- scripts/stop_components.py
+++ scripts/stop_components.py
@@ -3,25 +3,51 @@ import os, sys
 from constants import ASV_DEF, DLV_DEF
 
 def stopASV():
+    '''
+        Stop the ASV container.
+    '''
     print("Stopping ASV...")
+
     cmd = ["docker", "stop",  ASV_DEF["CONTAINER_NAME"]]
-    output = subprocess.run(
+    result = subprocess.run(
         cmd, 
         capture_output=True, 
         text=True
-    )    
+    )
+
+    if result.returncode != 0:
+        print(f"Failed to stop ASV container: {result.stderr}")
+        return False    
+    
     print("Stopped ASV service.")
+    return True
 
 def stopDLV():
+    '''
+        Stop the DLV container.
+    '''
     print("Stopping DLV...")
+    
     cmd = ["docker", "stop",  DLV_DEF["CONTAINER_NAME"]]
-    output = subprocess.run(
+    result = subprocess.run(
         cmd, 
         capture_output=True, 
         text=True
     )    
+
+    if result.returncode != 0:
+        print(f"Failed to stop DLV container: {result.stderr}")
+        return False   
+    
     print("Stopped DLV service.")
+    return True
+
+def main(argv):
+    if (not stopASV()):
+        return -1
+    
+    if (not stopDLV()):
+        return -1
 
 if __name__ == "__main__":
-    stopASV()
-    stopDLV()
+    sys.exit(main(sys.argv))
