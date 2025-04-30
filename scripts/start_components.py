80520f8979b5c6cca78021884946ae93081e4920
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-04-30 16:47:49 -0400

    Improve comments and structure of code.

--- scripts/start_components.py
+++ scripts/start_components.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
 import subprocess
 from utils import doesContainerExist, buildImage
@@ -7,14 +7,29 @@ import os
 import sys
 
 def buildImages():
-    buildImage(ASV_DEF["IMAGE_NAME"], ASV_DEF["IMAGE_PATH"], ASV_DEF["COMPONENT_PATH"])
-    buildImage(DLV_DEF["IMAGE_NAME"], DLV_DEF["IMAGE_PATH"], DLV_DEF["COMPONENT_PATH"])
-
+    '''
+        Build the images if they haven't been created.
+    '''
+    try:
+        buildImage(ASV_DEF["IMAGE_NAME"], ASV_DEF["IMAGE_PATH"], ASV_DEF["COMPONENT_PATH"])
+        buildImage(DLV_DEF["IMAGE_NAME"], DLV_DEF["IMAGE_PATH"], DLV_DEF["COMPONENT_PATH"])
+        return True
+    except Exception as e:
+        print(f"Failed to build images: {e}")
+        return False
 
 def createDirectories():
-    os.makedirs("data", exist_ok=True)
-    os.makedirs(ASV_DEF["DATA_DIR"], exist_ok=True)
-    os.makedirs(DLV_DEF["DATA_DIR"], exist_ok=True)
+    '''
+        Create the data directories mounted to the docker containers.
+    '''
+    try:
+        os.makedirs("data", exist_ok=True)
+        os.makedirs(ASV_DEF["DATA_DIR"], exist_ok=True)
+        os.makedirs(DLV_DEF["DATA_DIR"], exist_ok=True)
+        return True
+    except Exception as e:
+        print(f"Failed to create directories: {e}")
+        return False
 
 def startASV():
     '''
@@ -23,8 +38,14 @@ def startASV():
 
     print("Starting Automated System Viewer...")
 
+    try:
+        containerExists = doesContainerExist(ASV_DEF["CONTAINER_NAME"])
+    except Exception as e:
+        print("Failed check to see if ASV container exists: {e}")
+        return False
+
     # Check if the container exists and start it
-    if (doesContainerExist(ASV_DEF["CONTAINER_NAME"])):
+    if (containerExists):
         print("ASV container already exists.")
         result = subprocess.run(
             ["docker", "start", ASV_DEF["CONTAINER_NAME"]],
@@ -47,6 +68,7 @@ def startASV():
         "-v", "./data/asv:/app/dist", \
         ASV_DEF["IMAGE_NAME"] \
     ]
+
     result = subprocess.run(cmd, capture_output=True, text=True)
     if result.returncode != 0:
         print(f"Failed to start ASV container: {result.stderr}")
@@ -62,8 +84,14 @@ def startDLV():
     '''
     print("Starting Diagnostic Log Viewer...")    
 
+    try:
+        containerExists = doesContainerExist(DLV_DEF["CONTAINER_NAME"])
+    except Exception as e:
+        print("Failed check to see if DLV container exists: {e}")
+        return False
+
     # If the container exists, start it and return.
-    if (doesContainerExist(DLV_DEF["CONTAINER_NAME"])):
+    if (containerExists):
         print("DLV container already exists.")
         result = subprocess.run(
             ["docker", "start", DLV_DEF["CONTAINER_NAME"]],
@@ -86,6 +114,7 @@ def startDLV():
         "-v", "./data/dlv:/app/dist", \
         DLV_DEF["IMAGE_NAME"] \
     ]
+
     result = subprocess.run(cmd,capture_output=True, text=True)
     if result.returncode != 0:
         print(f"Failed to start DLV container: {result.stderr}")
@@ -97,8 +126,11 @@ def startDLV():
 
 
 def main(argv):
-    createDirectories()
-    buildImages()
+    if (not createDirectories()):
+        return -1
+    
+    if (not buildImages()):
+        return -1
     
     if (not startASV()):
         return -1
--- scripts/utils.py
+++ scripts/utils.py
@@ -2,29 +2,35 @@ import subprocess
 import json
 
 def doesContainerExist(name):
+    '''
+        Check if the given container exists.
+    '''
     cmd = ["docker", "ps", "-aq", "-f", f"name={name}"]
     output = subprocess.run(
         cmd, 
         capture_output=True, 
         text=True
     )
-
-    return len(output.stdout) > 0
+    containers = json.loads(output.stdout)
+    return len(containers) > 0
 
 def doesImageExist(name):
+    '''
+        Check if the given image exists.
+    '''
     cmd = ["docker", "image", "inspect", name]
     output = subprocess.run(
         cmd, 
         capture_output=True, 
         text=True
     )
-
     images = json.loads(output.stdout)
-
     return len(images) > 0
 
 def buildImage(imageName, dockerPath, srcPath):
-
+    '''
+        Build the image given the docker image and source path.
+    '''
     if not doesImageExist(imageName):
         cmd = [
             "docker",
