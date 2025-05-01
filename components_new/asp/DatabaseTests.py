68c654c59541bf3b37cf3696e272b461db33939e
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-05-01 18:20:36 -0400

    Add tests for adding to systems table and creating traces table.

--- components_new/asp/DatabaseTests.py
+++ components_new/asp/DatabaseTests.py
@@ -34,25 +34,47 @@ class DatabaseWriter:
             Create the table to store all the systems.
         '''
         sql = '''
-            CREATE TABLE IF NOT EXISTS SYSTEMSTABLE (
+            CREATE TABLE IF NOT EXISTS SYSTEMSTABLE 
+            (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 system_id VARCHAR(100) NOT NULL,
                 system_ver VARCHAR(100),
                 name VARCHAR(100),
                 description VARCHAR(100),
                 programs JSON,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
         '''
+        self.cursor.execute(sql)
 
+    def addSystem(self):
+        system_id = "1234"
+        system_ver = "0.0.1".replace(".","")
+        name = "Distributed Sorting System"
+        description = "System Description"
+        programs = json.dumps({"A":1})
+
+        sql = ''' INSERT INTO SYSTEMSTABLE(system_id, system_ver, name, description, programs)
+                VALUES(%s,%s,%s,%s,%s) '''
+        self.cursor.execute(sql, (system_id, system_ver, name, description, programs))
+        self.conn.commit()
+
+        sql = f'''
+            CREATE TABLE IF NOT EXISTS {system_id}_{system_ver}_traces 
+            (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                deployment_id VARCHAR(100) NOT NULL,
+                trace_id VARCHAR(100),
+                start_ts TIMESTAMP,
+                end_ts TIMESTAMP,
+                trace_type VARCHAR(100),
+                traces TEXT,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        '''
         self.cursor.execute(sql)
 
 
-    def addSystem(self):
-        programs = json.dumps({"A":1})
-        sql = ''' INSERT INTO SYSTEMSTABLE(system_id, system_ver, name, description, programs)
-                VALUES(%s,%s,%s,%s,%s) '''
-        self.cursor.execute(sql, ("1234", "0.0.1", "Distributed Sorting System", "desc", programs))
-        self.conn.commit()
 
 
 
