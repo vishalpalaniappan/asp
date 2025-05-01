bf7b94cb8c1d5615877c2cb97e88744bb9f98f6e
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-05-01 01:59:13 -0400

    Add sample program into asp container which writes to database.

--- components_new/asp/test.py
+++ components_new/asp/test.py
@@ -1,6 +1,57 @@
+import time
+import mysql.connector
+import sys
+
+time.sleep(5)
+
+global cursor, conn, count
+count = 0
 
 def writeToDatabase():
-    pass
+    while True:
+        time.sleep(10)
+        global count, cursor, conn
+        count += 1
+
+        insert_query = """
+        INSERT INTO data9 (name, email)
+        VALUES (%s, %s)
+        """
+
+        values = (str(count), str(count))
+        cursor.execute(insert_query, values)
+        conn.commit()
+
+def main(argv):
+    try:
+        global cursor, conn, count
+        conn = mysql.connector.connect(
+            host="mariadb-container",
+            user="root",
+            password="random-password",
+            database="asp-database",
+            port="3306"
+        )
+
+        cursor = conn.cursor()
+        cursor.execute("SELECT version();")
+        print(cursor.fetchone())
+
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS data9 (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(100),
+            email VARCHAR(100),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        ''')
+    except Exception as e:
+        print("EXCEPTION:", e)
+        return -1
+
+    writeToDatabase()
+    return 0
+
 
 if __name__ == "__main__":
-    writeToDatabase()
+    sys.exit(main(sys.argv))
