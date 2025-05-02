6a98c8fe8b961914949ff80f54a357129eb75a2a
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-05-02 00:01:18 -0400

    Watch for changes in the folder.

--- components_new/asp/SystemProcessor.py
+++ components_new/asp/SystemProcessor.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import time
 from cdl.Cdl import Cdl
 from pathlib import Path
 from database.EventWriter import EventWriter
@@ -14,29 +16,47 @@ class SystemProcessor:
         '''
         self.logFolder = logFolder
         self.logFiles = []
-        
+
+    def run(self):
         with DBClient() as db:
+            self.db = db
             self.eventWriter = EventWriter(db)
             self.sysWriter = SystemWriter(db)
-            self.parseSystemLogFiles()
+            self.monitorFolder()
 
-            # Run the trace assembler on the events saved to the ioEvents database.
-            TraceAssembler(db)
 
-    def parseSystemLogFiles(self):
+    def monitorFolder(self):
         '''
             Parse the system log files contained in folder.
         '''
-        files = os.listdir(self.logFolder)
+        visitedFiles = []
 
-        for logFileName in files:
-            if logFileName.endswith(".clp.zst"):
-                cdlFile = Cdl(os.path.join(self.logFolder, logFileName))
-                self.logFiles.append(cdlFile)
-                self.eventWriter.addEventsToDb(cdlFile)
-                self.sysWriter.write_file(cdlFile)
+        while True:
+            time.sleep(2)
+            current = os.listdir(self.logFolder)
+            newFiles = current - visitedFiles
 
-if __name__ == "__main__":
+            if len(newFiles) == 0:
+                continue
+
+            for logFileName in newFiles:
+                if logFileName.endswith(".clp.zst"):
+                    cdlFile = Cdl(os.path.join(self.logFolder, logFileName))
+                    self.logFiles.append(cdlFile)
+                    self.eventWriter.addEventsToDb(cdlFile)
+                    self.sysWriter.write_file(cdlFile)
+
+            # TODO: Only assemble the new nodes that were added
+            TraceAssembler(self.db)
+            
+            visitedFiles = current
+
+
+def main(argv):
     rootDir = Path(__file__).resolve().parents[0]
     SYSTEM_LOG_FILES = rootDir / "system_logs"
     processor = SystemProcessor(SYSTEM_LOG_FILES)
+    processor.run()
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
