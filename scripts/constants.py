4b9c0f443d0d1ed4850a39946b8d8a591baa482b
Vishal Palaniappan <101469936+vishalpalaniappan@users.noreply.github.com>
2025-05-02 23:22:49 -0400

    chore(docker): Rename all containers and images so that they have the prefix "asp". (#36)

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,6 +1,6 @@
 ASV_DEF = {
-    "CONTAINER_NAME": "asv-container",
-    "IMAGE_NAME": "asv-image",
+    "CONTAINER_NAME": "asp-asv-container",
+    "IMAGE_NAME": "asp-asv-image",
     "IMAGE_PATH": "docker-images/asv/Dockerfile",
     "COMPONENT_PATH": "components_new/asv",
     "PORT": 3012,
@@ -8,8 +8,8 @@ ASV_DEF = {
 }
 
 DLV_DEF = {
-    "CONTAINER_NAME": "dlv-container",
-    "IMAGE_NAME": "dlv-image",
+    "CONTAINER_NAME": "asp-dlv-container",
+    "IMAGE_NAME": "asp-dlv-image",
     "IMAGE_PATH": "docker-images/dlv/Dockerfile",
     "COMPONENT_PATH": "components_new/dlv",
     "PORT": 3011,
@@ -17,7 +17,7 @@ DLV_DEF = {
 }
 
 DB_DEF = {
-    "CONTAINER_NAME": "mariadb-container",
+    "CONTAINER_NAME": "asp-mariadb-container",
     "PORT": 3306,
     "DATABASE_NAME": "aspDatabase",
     "DATABASE_PASSWORD": "random-password",
@@ -26,7 +26,7 @@ DB_DEF = {
 
 ASP_DEF = {
     "CONTAINER_NAME": "asp-container",
-    "IMAGE_NAME": "asp-image",
+    "IMAGE_NAME": "asp-asp-image",
     "IMAGE_PATH": "docker-images/asp/Dockerfile",
     "COMPONENT_PATH": "components_new/asp",
     "DATA_DIR": "data/asp",
