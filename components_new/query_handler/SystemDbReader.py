93e5ffed444419dde39234402f608239dd111db5
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-05-03 02:54:02 -0400

    Add query handler component.

---
+++ components_new/query_handler/SystemDbReader.py
@@ -0,0 +1,126 @@
+import mysql.connector
+import os
+import time
+from datetime import datetime
+
+class SystemDbReader:
+    '''
+        This class will perform queries on the ASP database and return
+        the results.
+    '''
+
+    def __init__(self):
+        self.conn = self.get_connection()
+        self.cursor = self.conn.cursor()
+
+    def get_connection(self):
+        
+        isDocker = os.environ.get('IS_RUNNING_IN_DOCKER', False)
+
+        if isDocker:
+            host = "asp-mariadb-container"
+        else:
+            host = "localhost"
+
+        MAX_ATTEMPTS = 20
+        for attempt in range(MAX_ATTEMPTS):
+            try:
+                return mysql.connector.connect(
+                    host= host,
+                    user= "root",
+                    password= "random-password",
+                    database= "aspDatabase",
+                    port= "3306"
+                )
+            except Exception as e:
+                print(f"Attempt {attempt + 1}: {e}")
+                print("Trying again in 5 seconds.")
+                attempt += 1
+            time.sleep(5)
+
+        raise Exception(f"Tried to connect to database {attempt} times but failed.")
+            
+    def getColumns(self, tableName):
+        '''
+            Get the columns for a table.
+        '''
+        query = """
+            SELECT COLUMN_NAME
+            FROM INFORMATION_SCHEMA.COLUMNS
+            WHERE TABLE_NAME = %s
+        """
+        self.cursor.execute(query, (tableName,))
+        result = self.cursor.fetchall()
+
+        columns = []
+        for row in result:
+            columns.append(row[0])
+
+        return columns
+    
+    def addColumnNameToData(self, columns, data):
+        '''
+            Given the column names and the data, create 
+            an object which uses the column names as key.
+        '''
+        obj = {}
+        for index, columnValue in enumerate(data):
+            if isinstance(columnValue, datetime):
+                obj[columns[index]] = columnValue.timestamp()
+            else:
+                obj[columns[index]] = columnValue
+
+        return obj
+    
+    def getAllEntriesInTable(self, tableName):
+        '''
+            Given a table name, get all entries in the table.
+        '''
+        query = f'SELECT * FROM {tableName}'
+        self.cursor.execute(query)
+        rows = self.cursor.fetchall() 
+        columns = self.getColumns(tableName)
+
+        results = []
+        for row in rows:
+            results.append(self.addColumnNameToData(columns, row))
+
+        return results
+    
+    def getSystems(self):
+        '''
+            Get all systems in the database.
+        '''
+        return self.getAllEntriesInTable("SYSTEMTABLES")
+
+    def getDeployments(self, systemId, systemVersion):  
+        '''
+            Get all the deployments for the given system.
+        '''            
+        sysVerFormatted = systemVersion.replace(".","")
+        tableName = f"{systemId}_{sysVerFormatted}_deployments"
+        return self.getAllEntriesInTable(tableName)
+
+    def getTraces(self, systemId, systemVersion, deploymentId):
+        '''
+            Get all the traces given a system id, version and deployment id.            
+            # TODO: Add funtionality to filter traces by start and end timestamp.       
+        '''
+        sysVerFormatted = systemVersion.replace(".","")
+        tableName = f"{systemId}_{sysVerFormatted}_traces"
+
+        query = f'SELECT * FROM {tableName} WHERE deployment_id = %s'
+        self.cursor.execute(query, (deploymentId,))
+        rows = self.cursor.fetchall() 
+
+        columns = self.getColumns(tableName)
+
+        results = []
+        for row in rows:
+            results.append(self.addColumnNameToData(columns, row))
+
+        return results
+    
+if __name__ == "__main__":
+    a = SystemDbReader()
+    
---
+++ components_new/query_handler/queryFunctions.py
@@ -0,0 +1,82 @@
+import json
+from SystemDbReader import SystemDbReader
+
+reader = SystemDbReader()
+
+def handleUnknownMessage(message):
+    '''
+        Handle an unknown message.
+    '''
+    message["response"] = "Query did not contain a queryType key."
+    message["error"] = True
+    return message
+
+def handleGetSystems(message):
+    '''
+        Get all systems.
+    '''
+    try:
+        systems = reader.getSystems()
+        for system in systems:
+            system["deployments"] = reader.getDeployments(
+                systemId= system["system_id"],
+                systemVersion= system["version"]
+            )
+            system["programs"] = reader.getPrograms(
+                systemId= system["system_id"],
+                systemVersion= system["version"]
+            )
+        message["response"] = systems
+    except Exception as e:
+        message["error"] = True
+        message["response"] = f"Database error: {e}"
+
+    return message
+
+def handleGetTraces(message):
+    '''
+        Get all traces given a system id, version and deployment id.
+    '''
+    if ("data" not in message):
+        message["response"] = "Query does not contain the required data key."
+        message["error"] = True
+        return message
+    
+    data = message["data"]
+
+    # TODO: Add missing key response to a list and return all of them.
+    if ("systemId" not in data):
+        message["response"] = "Query does not contain a system id."
+        message["error"] = True
+        return message
+
+    if ("systemVersion" not in data):
+        message["response"] = "Query does not contain a system version."
+        message["error"] = True
+        return message
+
+    if ("deploymentId" not in data):
+        message["response"] = "Query does not contain a deployment id."
+        message["error"] = True
+        return message
+    
+    try:    
+        message["response"] = reader.getTraces(
+            systemId= message["data"]["systemId"],
+            systemVersion= message["data"]["systemVersion"],
+            deploymentId= message["data"]["deploymentId"],
+        )
+    except Exception as e:
+        message["error"] = True
+        message["response"] = f"Database error: {e}"
+
+    return message
+
+
+def handleUnknownQueryType(message):
+    '''
+        Handle an unkown query type.
+    '''
+    message["response"] = f"Unknown message type: {message['queryType']}"
+    message["error"] = True
+    return message
