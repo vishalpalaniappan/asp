48248ed8520641c15b756ada0c6ea0da4a9c2158
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-05-09 01:45:55 -0400

    Remove mysql.connector dependency from startup scripts.

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -18,7 +18,6 @@ DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
   python3 \
   python3-pip \
   python3-venv \
-  python3-mysql.connector \
   software-properties-common \
   unzip
 
